e5645cc31c326ab46f0b3b11ee0b1ae39df5236d
siddhartha.kumar <siddhartha.kumar@INGGNLT35LM693.INFO.CORP>
2022-08-11 11:05:27 +0530

    testCommit

--- src/com/basic/concepts/Test.java
+++ src/com/basic/concepts/Test.java
@@ -2,14 +2,24 @@ package com.basic.concepts;
 
 public class Test {
 	
+	static int integer= 1;
+	
 	public static void main(String[] args) {
 	
 		// new keyword
 		Cow cow = new Cow();
 		
+		int i = 0;
+		
+		if( 1==1) {
+		
+			int u = 0;
+		}
+		
+		
 		Long returnValue = cow.eat("grass");
 		
-		System.out.println("returnValue : " + returnValue);
+		System.out.println("returnValue : " + returnValue + " class level variable : " + integer);
 		
 		Gir cow1 = new Gir();//inheritance
 		

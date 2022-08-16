36f3bdf8266d5af6a3b6b0a951eb2013ca686756
siddhartha.kumar <siddhartha.kumar@INGGNLT35LM693.INFO.CORP>
2022-08-16 14:22:07 +0530

    Revert "testCommit"
    This reverts commit e5645cc31c326ab46f0b3b11ee0b1ae39df5236d.

--- src/com/basic/concepts/Test.java
+++ src/com/basic/concepts/Test.java
@@ -2,24 +2,14 @@ package com.basic.concepts;
 
 public class Test {
 	
-	static int integer= 1;
-	
 	public static void main(String[] args) {
 	
 		// new keyword
 		Cow cow = new Cow();
 		
-		int i = 0;
-		
-		if( 1==1) {
-		
-			int u = 0;
-		}
-		
-		
 		Long returnValue = cow.eat("grass");
 		
-		System.out.println("returnValue : " + returnValue + " class level variable : " + integer);
+		System.out.println("returnValue : " + returnValue);
 		
 		Gir cow1 = new Gir();//inheritance
 		
